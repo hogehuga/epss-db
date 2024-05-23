93e67b4e313413aa5ccfd5a8b8dfbaa989247806
hogehuga <hogehuga@gmail.com>
2024-05-23 18:00:40 +0900

    fix typo

--- update-all.sh
+++ update-all.sh
@@ -4,6 +4,6 @@
 echo "Update EPSS data"
 /opt/epss-db/update-epss.sh
 #echo"Update KEV Catalog data"
-#/opt/epss-db/uupdate-kev.sh
+#/opt/epss-db/update-kev.sh
 #echo "Update Vulnrichment data"
-#/opt/epss-db/uupdate-vulnrich.sh
+#/opt/epss-db/update-vulnrich.sh
