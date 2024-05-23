0a44645417742bb3454c7ceeb8cead0e97e06bc6
hogehuga <hogehuga@gmail.com>
2024-05-23 17:34:13 +0900

    update

--- update-vulnrich.sh
+++ update-vulnrich.sh
@@ -2,24 +2,27 @@
 
 # env
 targetdir="/opt/epss-db/vulnrichment"
-outfile="/opt/epss-db/epss-data/vulnrichment.csv"
-
+outfile="/var/lib/mysql-files/vulnrichment.csv"
 # file pre delete
+echo "rmfile"
 if [ -e $outfile ]; then
   rm $outfile
+  echo "rm"
 fi
 
 # update vulnrichment
+echo "---"
+echo "Update Vulnrichment reposiotry."
 cd $targetdir
 git pull
 
 # create CSV data from Vulnrichment JSON
+echo "---"
+echo "Create import csv file."
 find $targetdir -name "*.json" -exec /opt/epss-db/subprogram/vulnrichUpdate.sh {} \;
 
 # import CSV data
-mysql --defaults-extra-file=/opt/epss-db/my.cnf epssdb -e "load data infile '$outfile' into table richment fields terminated by ',' enclosed by '\"' (cveId, adpCweId, adpSSVCExploitation, adpSSVCAutomatable, adpSSVCTechImpact, adpKEVDateadded, adpKEVRef, adp31Score, adp31Severity, adp31Vector, cna31Score, cna31Severity, cna31VectorString, cna40Score, cna40Severity, cna40Vector);"
-
-# file delete
-if [ -e $outfile ]; then
-  rm $outfile
-fi
+echo "---"
+echo "Import data to mysql"
+mysql --defaults-extra-file=/opt/epss-db/my.cnf -u root epssdb -e "load data infile '$outfile' into table richment fields terminated by ',' enclosed by '\"' (cveId, adpCweId, adpSSVCExploitation, adpSSVCAutomatable, adpSSVCTechImpact, adpKEVDateadded, adpKEVRef, adp31Score, adp31Severity, adp31Vector, cna31Score, cna31Severity, cna31VectorString, cna40Score, cna40Severity, cna40Vector);"
+echo "- finish"
